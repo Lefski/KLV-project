800ce0c6e971b467b8baf35d204b7c2c2b14f05b
watisluv1910 <nasevich.vlasd.03@gmail.com>
2023-03-14 11:25:35 +0300

    Modified eslint config file.

--- react-ui/.eslintrc.js
+++ react-ui/.eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'browser': true,
+        'es2021': true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "google"
+    'extends': [
+        'plugin:react/recommended',
+        'google',
     ],
-    "overrides": [],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    'overrides': [],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module',
     },
-    "plugins": [
-        "react"
+    'plugins': [
+        'react',
     ],
-    "rules": {
-        "indent": "off",
-        "react/react-in-jsx-scope": "off",
-        "linebreak-style": ["error", process.platform === "win32" ? "windows" : "unix"]
-    }
-}
+    'rules': {
+        'indent': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'linebreak-style':
+            ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+    },
+};
