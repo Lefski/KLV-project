ec246ca7a0e27145d859fb8edc3f3089aa2978df
watisluv1910 <nasevich.vlasd.03@gmail.com>
2023-04-06 19:35:03 +0300

    Added AuthenticationProvider and PasswordEncoder implementations to ApplicationConfig

--- springboot-backend/src/main/java/com/app/springbackend/ApplicationConfig.java
+++ springboot-backend/src/main/java/com/app/springbackend/ApplicationConfig.java
@@ -4,8 +4,12 @@ import com.app.springbackend.repo.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,5 +27,28 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    /**
+     Returns an instance of {@link AuthenticationProvider} that uses
+     the {@link UserDetailsService} and {@link PasswordEncoder}
+     to authenticate users.
+     @return the {@link AuthenticationProvider} instance.
+     */
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    /**
+     Returns an instance of {@link PasswordEncoder} that uses BCrypt algorithm.
+     @return the {@link PasswordEncoder} instance.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
