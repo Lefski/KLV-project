9c3765e89d8ef2ee54154e75a01b63f186e1437a
watisluv1910 <nasevich.vlasd.03@gmail.com>
2023-04-22 15:32:36 +0300

    Added Spring @Repository annotation to UserRepository

--- springboot-backend/src/main/java/com/app/springbackend/repo/UserRepository.java
+++ springboot-backend/src/main/java/com/app/springbackend/repo/UserRepository.java
@@ -1,11 +1,12 @@
 package com.app.springbackend.repo;
 
 import com.app.springbackend.model.user.UserEntity;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
