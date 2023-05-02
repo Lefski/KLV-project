d7a8dfef1c60630e248b977801d6d5a23e6aea79
watisluv1910 <nasevich.vlasd.03@gmail.com>
2023-05-03 00:49:39 +0300

    Add UserDetails and UserDetailsService implementations

---
+++ springboot-backend/src/main/java/com/app/springbackend/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.app.springbackend.security.services;
+
+import com.app.springbackend.model.user.User;
+import com.app.springbackend.repo.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
