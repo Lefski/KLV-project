33cbd15a5c69be664d38fd2097d13488bd3c24aa
watisluv1910 <nasevich.vlasd.03@gmail.com>
2023-05-03 00:21:02 +0300

    Add test endpoints for role-based access check

--- springboot-backend/src/main/java/com/app/springbackend/controller/TestController.java
+++ springboot-backend/src/main/java/com/app/springbackend/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.app.springbackend.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Secured endpoint here");
+    public ResponseEntity<String> publicAccess() {
+        return ResponseEntity.ok("Public content");
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('USER', 'MODERATOR', 'ADMIN')")
+    public ResponseEntity<String> userAccess() {
+        return ResponseEntity.ok("User content");
+    }
+
+    @GetMapping("/moderator")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public ResponseEntity<String> moderatorAccess() {
+        return ResponseEntity.ok("Moderator content");
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> adminAccess() {
+        return ResponseEntity.ok("Admin content");
     }
 }
