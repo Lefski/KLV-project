ffd4d35a92a8833313ca1b3c3d0f8842bdcd7a1f
watisluv1910 <nasevich.vlasd.03@gmail.com>
2023-05-03 00:11:50 +0300

    Add initial check to register method

--- springboot-backend/src/main/java/com/app/springbackend/controller/AuthenticationController.java
+++ springboot-backend/src/main/java/com/app/springbackend/controller/AuthenticationController.java
@@ -1,16 +1,14 @@
 package com.app.springbackend.controller;
 
 
-import com.app.springbackend.security.auth.AuthenticationRequest;
-import com.app.springbackend.security.auth.AuthenticationResponse;
-import com.app.springbackend.security.auth.AuthenticationService;
-import com.app.springbackend.security.auth.RegisterRequest;
+import com.app.springbackend.payload.request.AuthenticationRequest;
+import com.app.springbackend.payload.response.MessageResponse;
+import com.app.springbackend.repo.UserRepository;
+import com.app.springbackend.security.services.AuthenticationService;
+import com.app.springbackend.payload.request.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -18,17 +16,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private final UserRepository userRepository;
     private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<?> register(
             @RequestBody RegisterRequest request
     ) {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken"));
+        }
+
+        if (userRepository.existsByUserEmail(request.getUserEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use"));
+        }
+
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> authenticate (
+    public ResponseEntity<?> authenticate (
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
