5a151751719e70e49daa03346bf375ba401f829e
watisluv1910 <nasevich.vlasd.03@gmail.com>
2023-05-03 12:32:20 +0300

    Rework token validity check method

--- springboot-backend/src/main/java/com/app/springbackend/security/jwt/JwtUtils.java
+++ springboot-backend/src/main/java/com/app/springbackend/security/jwt/JwtUtils.java
@@ -1,11 +1,12 @@
 package com.app.springbackend.security.jwt;
 
 import com.app.springbackend.security.services.UserDetailsImpl;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,6 +26,8 @@ import java.util.function.Function;
  */
 @Service
 public class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
     @Value("${klv.app.sign-key}")
     private String SIGN_KEY;
@@ -110,8 +113,25 @@ public class JwtUtils {
      @return true if the token is valid for the user, false otherwise.
      */
     public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSignInKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
     }
 
     /**
